f03931c78b97a1c70c6dbb02b354d4258c72e234
fr31b3u73r <fr31b3u73r@gmail.com>
2018-01-28 10:11:49 +0100

    Added implementation, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# miThermyHygro
-Xiaomi BLE Thermo- &amp; Hygrometer implementation for Domoticz
+# miThermoHygro
+[![Python](https://img.shields.io/badge/python-2-brightgreen.svg)]() [![Donate](https://img.shields.io/badge/donate-PayPal-brightgreen.svg)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=5654A67GA3GHA)
+
+**Important information: This library was developed for working with FW version 00.00.66. Xiaomi might change interface in future FW updates!**
+
+*This python script allows you interfacing Xiaomi BLE temperature and humidity sensors with Domoticz. To be able to use it your device needs a bluetooth hardware that supports BLE, Python (2) as well as external libraries as defined in imports. Tested on Raspberry Pi 3.*
+
+## Setup
+First you need to know the MAC addresses of your sensor devices (can be easily found using hcitool on PC/Mac or smarthone BLE scan apps).
+Next create a new hardware device in Domoticz using type "dummy" and add the according amount of your sensors as devices of type "Temp + Humidity" to it. Afterwards open the script and edit the Domoticz configuration to match your setup. Next add your sensors (with the MAC addresses you got above) and assign them to the according device Idx of domoticz. You can add as many sensors as you want (of course also only one is possible).
+
+That´s it! Now whenever you run the Python script the sensor data in Domoticz gets updated. For continuous polling just create a cronjob for example on your device calling the Python script.
+
+## Supported values
+* temperature
+* humidity
+* battery level
+
+## Thanks
+A big thanks to [Dirk (distel) of the FHEM forum](https://forum.fhem.de/index.php/topic,82249.0.html) for reverse engineering the sensor!
+
---
+++ domoticz_poller.py
@@ -0,0 +1,63 @@
+import binascii
+import base64
+import requests
+from bluepy.btle import UUID, Peripheral, ADDR_TYPE_PUBLIC, DefaultDelegate
+
+# domoticz configuration
+DOMOTICZ_SERVER   = "127.0.0.1:8080"
+DOMOTICZ_USERNAME = "user"
+DOMOTICZ_PASSWORD = "password"
+
+# sensor dictionary to add own sensors
+# key is MAC of BT sensor, value is sensor ID
+sensor_dict = {}
+sensor_dict["4C:65:A8:D0:XX:XX"] = 1
+sensor_dict["4C:65:A8:D0:XX:XX"] = 2
+
+# constants for polling sensor; do not change!
+BATTERY_HANDLE = 0x0018
+TEMP_HUM_WRITE_HANDLE = 0x0010
+TEMP_HUM_READ_HANDLE = 0x000E
+TEMP_HUM_WRITE_VALUE = bytearray([0x01, 0x10])
+
+battery_level = -1
+sensor_id = -1
+
+class TempHumDelegate(DefaultDelegate):
+	def __init__(self):
+		DefaultDelegate.__init__(self)
+
+	def handleNotification(self, cHandle, data):
+		if (cHandle == TEMP_HUM_READ_HANDLE):
+			temperature = data.split(" ")[0][2:]
+			humidity = data.split(" ")[1][2:]
+			if (sensor_id != -1 and battery_level > -1):
+				request_url = 'http://' + DOMOTICZ_SERVER + '/json.htm?type=command&param=udevice&idx=' + str(sensor_id) + '&nvalue=0&svalue=' + temperature + ';' + humidity + '&battery=' + str(battery_level)
+				send_to_domoticz(request_url)
+
+def send_to_domoticz(url):
+	requests.get(url, auth=(DOMOTICZ_USERNAME, DOMOTICZ_PASSWORD))
+
+def get_battery_value():
+	battery_value = 0;
+	try:
+		battery_value = p.readCharacteristic(BATTERY_HANDLE)
+	finally:
+		return int(binascii.b2a_hex(battery_value), 16)
+
+def handle_temp_hum_value():
+	p.writeCharacteristic(TEMP_HUM_WRITE_HANDLE, TEMP_HUM_WRITE_VALUE)
+	while True:
+		if p.waitForNotifications(1.0):
+			break
+
+for key, value in sensor_dict.iteritems():
+	try:
+		sensor_id = value
+		p = Peripheral(key)
+		p.withDelegate(TempHumDelegate())
+		battery_level = get_battery_value()
+		handle_temp_hum_value()
+		p.disconnect()
+	except:
+		pass
